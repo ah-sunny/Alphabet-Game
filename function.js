5bc5db2730997ea2a043034a13c001b939bb366c
AH-sunny <sunnycse03@gmail.com>
2024-02-15 15:36:59 +0600

    score update

--- function.js
+++ function.js
@@ -15,8 +15,66 @@ function forDisplayLetterById(disElementId){
 //display
     const letterDisplay =document.getElementById(disElementId);
     letterDisplay.innerText = alphabet;
-    
+
     const keyColor = document.getElementById(alphabet);
     keyColor.style.backgroundColor = 'orange';
 }
+//score & life and continue game & bg remove
+function handleKeyboarKey(event){
+    const preeKey = event.key;
+    //console.log("press key",preeKey);
+
+    const letterDisplay =document.getElementById('for_dis_letter');
+    const currentShowLetter = letterDisplay.innerText;
+    //console.log(currentShowLetter);
+
+    if( currentShowLetter === preeKey){
+        console.log('good job')
+
+        const scoreTextElement = document.getElementById('scode_num');
+        const scoreText = scoreTextElement.innerText ;
+        const score = parseInt(scoreText);
+
+        var presentScore = score + 1;
+        scoreTextElement.innerText = presentScore ;
+       
+        forDisplayLetterById('for_dis_letter');
+
+        const keyColor = document.getElementById(preeKey);
+        keyColor.style.backgroundColor = null;
+
+    }else{
+        console.log('bad job')
+        const lifeTextElement = document.getElementById('life_num');
+        const lifeText = lifeTextElement.innerText;
+        const lifeNumber = parseInt(lifeText);
+
+        const presentLife = lifeNumber - 1 ;
+        
+        if(presentLife === 0){
+            gameOver(); 
+            
+        }
+        lifeTextElement.innerText = presentLife ;
+    }
+}
+
+function gameOver(){
+    hiddenAndViewById('displayPart','scorePart');
+    const finalSocreElemet = document.getElementById('final_score');
+    const getscoreElement = document.getElementById('scode_num');
+    const getScore = getscoreElement.innerText;
+    finalSocreElemet.innerText = getScore;
+
+    //remove background current elemet
+    const lastkeyElement = document.getElementById('for_dis_letter');
+    const lastkeyLetter = lastkeyElement.innerText;
+
+    const lastKeyboardElemnt = document.getElementById(lastkeyLetter);
+    lastKeyboardElemnt.style.backgroundColor = null;
+
+    
+
+}
+
 
--- mainfunc.js
+++ mainfunc.js
@@ -3,40 +3,15 @@ function gameStart(){
     hiddenAndViewById('startPart','displayPart');
     forDisplayLetterById('for_dis_letter');
     
+    hiddenAndViewById('scorePart','displayPart');
+
+    //score and life reset
+    const scoreTextid = document.getElementById('scode_num');
+    scoreTextid.innerText = 0;
+    const setLife = document.getElementById('life_num');
+    setLife.innerText = 4;
+
 }
 
 document.addEventListener('keyup',handleKeyboarKey);
 
-function handleKeyboarKey(event){
-    const preeKey = event.key;
-    //console.log("press key",preeKey);
-
-    const letterDisplay =document.getElementById('for_dis_letter');
-    const currentShowLetter = letterDisplay.innerText;
-    //console.log(currentShowLetter);
-
-    if( currentShowLetter === preeKey){
-        console.log('good job')
-
-        const scoreTextElement = document.getElementById('scode_num');
-        const scoreText = scoreTextElement.innerText ;
-        const score = parseInt(scoreText);
-
-        const presentScore = score + 1;
-        scoreTextElement.innerText = presentScore ;
-       
-        forDisplayLetterById('for_dis_letter');
-
-        const keyColor = document.getElementById(preeKey);
-        keyColor.style.backgroundColor = null;
-
-    }else{
-        console.log('bad job')
-        const lifeTextElement = document.getElementById('life_num');
-        const lifeText = lifeTextElement.innerText;
-        const lifeNumber = parseInt(lifeText);
-
-        const presentLife = lifeNumber - 1 ;
-        lifeTextElement.innerText = presentLife ;
-    }
-}
